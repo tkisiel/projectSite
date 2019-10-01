0ba7d0abf00393dc5120e17d6b734e102608bcdf
tkisiel <42995644+tkisiel@users.noreply.github.com>
2019-10-01 10:33:14 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# projectSite
+<<<<<<< HEAD
+# projectSite
+# projectSite
+=======
+>>>>>>> 534a7ae5426a3524e8d80e0e33ace398194f7f20
---
+++ js/site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $(".mobilMenu").click(function () {
+        $(".mobils").toggleClass("show");
+    });
+
+    $(function () {
+        $('.selects').change(function () {
+            console.log($(this).val());
+        }).multipleSelect({
+            width: '100%'
+        });
+    });
+});
